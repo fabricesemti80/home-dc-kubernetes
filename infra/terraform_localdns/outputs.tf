a348472e99b1cfafd932c8ac8faabad5c163f2a6
Fabrice Semti <emilfabrice@gmail.com>
2026-08-03 17:27:07 +0100

    Add HomeTube video downloader (#255)

--- infra/terraform_localdns/outputs.tf
+++ infra/terraform_localdns/outputs.tf
@@ -50,5 +50,10 @@ output "kubernetes_internal_dns_records" {
       type   = unifi_dns_record.tdarr_internal.type
       record = unifi_dns_record.tdarr_internal.record
     }
+    hometube = {
+      name   = unifi_dns_record.hometube_internal.name
+      type   = unifi_dns_record.hometube_internal.type
+      record = unifi_dns_record.hometube_internal.record
+    }
   }
 }
